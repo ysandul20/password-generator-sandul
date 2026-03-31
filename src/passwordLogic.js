b4a99c3a5c9b836a67584e1842dc7582324388f3
ysandul <ysandul19@gmail.com>
2026-03-31 20:25:16 +0300

    fix bug

--- src/passwordLogic.js
+++ src/passwordLogic.js
@@ -2,12 +2,11 @@ const LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
 const NUMBERS = "0123456789";
 
 export const generatePassword = (length = 8) => {
-   // Рефакторинг: додали цифри для надійності
    const allChars = LOWERCASE_CHARS + NUMBERS;
    let password = "";
 
-   // УВАГА БАГ: цикл генерує на 1 символ менше, ніж треба (length - 1)
-   for (let i = 0; i < length - 1; i++) {
+   // FIXED: змінили length - 1 назад на length
+   for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * allChars.length);
       password += allChars[randomIndex];
    }
