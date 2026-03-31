10ee09bfb68040e46a004206d409ecc5504b54fb
ysandul <ysandul19@gmail.com>
2026-03-31 20:19:00 +0300

    add core logic

--- src/passwordLogic.js
+++ src/passwordLogic.js
@@ -1,3 +1,9 @@
-export const generatePassword = () => {
-   return "placeholder";
+export const generatePassword = (length = 8) => {
+   const chars = "abcdefghijklmnopqrstuvwxyz";
+   let password = "";
+   for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      password += chars[randomIndex];
+   }
+   return password;
 };
