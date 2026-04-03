d9d40532c2ec03f8b5e05d12b70482861c7db7b6
ysandul <ysandul19@gmail.com>
2026-04-03 21:20:08 +0300

    add CI pipeline with broken test

---
+++ src/passwordLogic.test.js
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { generatePassword } from "./passwordLogic";
+
+test("generatePassword returns password of correct length", () => {
+   const password = generatePassword(10);
+
+   // НАВМИСНА ПОМИЛКА: ми очікуємо довжину 99, хоча замовляли 10
+   expect(password.length).toBe(99);
+});
